e5d680b55803540e1e2ef123bd1db7bf5a3a7f49
Williams <wil_uh_ah94@yahoo.com>
2020-03-28 17:49:06 -0400

    primera prueba en la nube para el frontend de py ex-final

---
+++ precache-manifest.c0b195d2d9f96edc0de92823bc044ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc38bbd1c45f8283434b5b5ae551908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f08709063cc6e100919",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "974c8c7aaa8384c4565f",
+    "url": "/static/css/main.38f1805e.chunk.css"
+  },
+  {
+    "revision": "2f08709063cc6e100919",
+    "url": "/static/js/2.281f296b.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/static/js/2.281f296b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974c8c7aaa8384c4565f",
+    "url": "/static/js/main.e5ef1f8d.chunk.js"
+  },
+  {
+    "revision": "8182a387c804e86d8e73",
+    "url": "/static/js/runtime-main.b810a271.js"
+  }
+]);
